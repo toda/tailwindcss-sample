9401daabf8c0be85b591f31211855530700c864a
toda <mugiwarano.tody@gmail.com>
2023-10-30 09:14:27 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    backgroundImage: {
+      "sample-bg": "url('../src/img/kikuya-ramen.jpg')",
+    },
+  },
+  plugins: [],
+}
